752b166968639da5bcb97bbd5146e429b02a0b9b
Bgudenas <bgudena@clemson.edu>
2017-11-13 16:56:42 -0500

    added fasta loop

--- src/FASTA_features.R
+++ src/FASTA_features.R
@@ -2,6 +2,19 @@
 # DeepLocal - Fasta pre-processing  ---------------------------------------
 # Fasta to feature set
 library(seqinr)
-seq = read.fasta("./Data/Homo_sapiens_SPATA21_203_sequence.fa")
+library(stringr)
+seq = read.fasta("./Data/Homo_sapiens_SPATA21_203_sequence.fa", forceDNAtolower = FALSE, as.string = TRUE)
 
-for ( i in )
+
+
+df = matrix(nrow= length(seq), ncol =2)
+colnames(df) = c( "cdna", "transcript_biotype")
+for ( i in seq){
+  df[i, 1] = as.character(seq[[i]])
+  
+  biotype = attr(seq[[i]], "Annot")
+  df[i, 2] = str_split(biotype, "cdna:")[[1]][2]
+}
+  
+  
+  
