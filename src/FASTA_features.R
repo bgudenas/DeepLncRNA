3d6ee0934c2312ab60820ebdb8d618c1251ef828
Bgudenas <bgudena@clemson.edu>
2017-11-13 16:34:30 -0500

    create features from FASTA cDNA

---
+++ src/FASTA_features.R
@@ -0,0 +1,7 @@
+
+# DeepLocal - Fasta pre-processing  ---------------------------------------
+# Fasta to feature set
+library(seqinr)
+seq = read.fasta("./Data/Homo_sapiens_SPATA21_203_sequence.fa")
+
+for ( i in )
