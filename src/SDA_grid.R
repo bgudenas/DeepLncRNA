ca2d5fb3cc708d5c827cc5643eb7868fbad3706b
bgudenas <bgudena@clemson.edu>
2017-12-04 17:55:54 -0500

    Bigger Architextures

--- src/SDA_grid.R
+++ src/SDA_grid.R
@@ -3,44 +3,25 @@ library(h2o)
 set.seed(54321)
 
 # Import data -------------------------------------------------------------
-df = readRDS("../Data/Unsupervised_train.rds")
-Super = readRDS("../Data/Supervised_train.rds")
-df$transcript_biotype = as.factor(df$transcript_biotype)
-Super$transcript_biotype = as.factor(Super$transcript_biotype)
+df = readRDS("./Unsupervised_train.rds")
+Super = readRDS("./Training_set.rds")
 
-df = df[ , -c(3, 5)]
-Super = Super[ , -5]
+localH2O = h2o.init(ip="localhost", port = 54321, startH2O = TRUE, nthreads = 20, max_mem_size = "78G")
 
+unsup = as.h2o(df[ ,-1])
 
-localH2O = h2o.init(ip="localhost", port = 54321, startH2O = TRUE, nthreads = 40, max_mem_size = "148G")
-
-#dat_h2o = as.h2o(df)
-
-inTrain = sample(1:nrow(Super), .7*nrow(Super) )
-
-train_raw = Super[inTrain, ]
-test_raw = Super[-inTrain, ]
-
-## Remove any testing instances from unsupervised DF for SDA
-df = df[is.na(match(rownames(df), rownames(test_raw))), ]
-unsup = as.h2o(df)
-
-train_hex = as.h2o(train_raw)
-test_hex = as.h2o(test_raw)
-
-
+train_hex = as.h2o(Super)
 train <- train_hex[,-1]
-test  <- test_hex [,-1]
 
 # Build SDAs --------------------------------------------------------------
 # test using diff architectures
-layer1 = c(3000,1500,750,350)
-layer_ov =c(2000,1000,500,250)
-layer_slim =c(500,500,500,500)
+layer1 = c(1500,1500,1500,1500)
+layer_ov =c(4000,2000,1000,500)
+layer_slim =c(3000,1500,750,350)
 
-args1 <- list(activation= "Tanh", epochs=10, input_dropout_ratio = 0)
-args2 <- list(activation= "Tanh", epochs=10, input_dropout_ratio = 0.1)
-args3 <- list(activation= "Tanh", epochs=10, input_dropout_ratio = 0.25)
+args1 <- list(activation= "Tanh", epochs=10, input_dropout_ratio = 0.1, mini_batch_size = 10)
+args2 <- list(activation= "Tanh", epochs=10, input_dropout_ratio = 0.25, mini_batch_size = 10)
+args3 <- list(activation= "Tanh", epochs=10, input_dropout_ratio = 0.4, mini_batch_size = 10)
 
 #layer1
 ael1 <- get_stacked_ae_array(unsup, layer1, args1)
@@ -69,7 +50,7 @@ train_sl1 <- apply_stacked_ae_array(train, aesl1)
 train_sl2 <- apply_stacked_ae_array(train, aesl2)
 train_sl3 <- apply_stacked_ae_array(train, aesl3)
 
-save.image("../Data/SDA_grid.RData")
+save.image("./SDA_grid.RData")
 
 
 SDA_rf(train_ael1, args1)
